d541fb3a8a4b70d4af0bd83cb8a7ae9050926a7c
Saggi Mizrahi <ficoos@gmail.com>
2013-10-14 03:29:11 +0300

    Initial Commit
    Signed-off-by: Saggi Mizrahi <ficoos@gmail.com>

---
+++ logify.js
@@ -0,0 +1,37 @@
+(function () {
+    /* Order is important, ambiguity is resolved by precedence */
+    var symbol_map = [
+        [(/\<-\>/g), "↔"],
+        [(/\<=\>/g), "↔"],
+        [(/\-\>/g), "→"],
+        [(/&/g), "∧"],
+        [(/\^/g), "∧"],
+        [(/\|\|/g), "∨"],
+        [(/V/g), "∨"],
+        [(/!/g), '¬'],
+        [(/~/g), '¬'],
+        [(/=/g), '≡']
+    ];
+
+    var activeElement = document.activeElement;
+
+    var selectionStart = activeElement.selectionStart;
+    var selectionEnd = activeElement.selectionEnd;
+
+    var prefix = activeElement.value.substring(0, selectionStart);
+    var suffix = activeElement.value.substring(selectionEnd,
+                                               activeElement.value.length);
+    var selectedText = activeElement.value.substring(selectionStart,
+                                                     selectionEnd);
+
+    var resultText = selectedText;
+    for (var i = 0; i < symbol_map.length; i++) {
+        var mapping = symbol_map[i];
+        var asciiSymbol = mapping[0];
+        var unicodeSymbol = mapping[1];
+        resultText = resultText.replace(asciiSymbol, unicodeSymbol);
+    }
+
+    activeElement.value = prefix + resultText + suffix;
+})();
+
